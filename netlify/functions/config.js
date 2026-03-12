3ba7ed6807ae437fcd29f5255b918c075cc3143c
colonyroofers <103861114+colonyroofers@users.noreply.github.com>
2026-03-12 19:10:11 -0400

    Replace Firebase config with environment variables

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -3,13 +3,13 @@ exports.handler = async () => {
     statusCode: 200,
     headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
     body: JSON.stringify({
-      apiKey: "AIzaSyCawWMq5X2OpWsmplOYxrR6bhyHeAzXKFw",
-      authDomain: "colony-inspection-app.firebaseapp.com",
-      projectId: "colony-inspection-app",
-      storageBucket: "colony-inspection-app.firebasestorage.app",
-      messagingSenderId: "869437682472",
-      appId: "1:869437682472:web:6df5166ffd01fe4209d61d",
-      measurementId: "G-1ZVLVL06CB"
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
     }),
   };
 };
